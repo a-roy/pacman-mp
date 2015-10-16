206ae7f8298475b69c360576a591d4ccf185c365
Aneesh Roy <aroy@tcd.ie>
2015-10-16 14:31:23 +0100

    Wrote documentation for graphics code

--- SFData.h
+++ SFData.h
@@ -8,12 +8,20 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+//! Contains data loaded by SFML
 class SFData
 {
 	public:
+		//! SFML RenderWindow
 		static sf::RenderWindow *Window;
+		//! SFML Texture database, where keys are file paths
 		static std::map<std::string, sf::Texture> Textures;
+		//! SFML Sprite database
 		static std::vector<sf::Sprite> Sprites;
 
+		//! Get a texture for the image at the given path, loading it into the
+		//! database if it has not already been loaded.
+		//!
+		//! \path A path to an image file
 		static const sf::Texture &GetTexture(std::string path);
 };
--- Sprite.h
+++ Sprite.h
@@ -6,9 +6,13 @@
 #include <vector>
 #include "Animation.h"
 
+//! Represents a drawable animated sprite
 class Sprite
 {
 	public:
+		//! Holds an index referring to a library-specific database with
+		//! rendering information for this sprite (e.g. SFData::Sprites)
 		int Index;
+		//! Contains animation information for this sprite
 		std::vector<Animation> Animations;
 };
