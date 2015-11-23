01fe9f64e138783954e1aaac0d3ad55568ca7caa
Aneesh Roy <aroy@tcd.ie>
2015-11-23 18:37:29 +0000

    Reduced sprite dimensions to 16x16

--- main.cpp
+++ main.cpp
@@ -57,12 +57,16 @@ int main()
 	item3.Function = Exiting;
 	Data::MainMenuData.MenuItems.push_back(item3);
 
-	Renderer::Scale = 8.0f;
+	Renderer::Scale = 4.0f;
 	Renderer::CreateWindow(1280, 720, "My window");
 
-	Data::GameplayData.PacSprite.Index = Renderer::CreateSprite("../pacman.png");
+	Data::GameplayData.PacmanSprite.Index =
+		Renderer::CreateSprite("../pacman.png");
+	Data::GameplayData.GhostSprite.Index =
+		Renderer::CreateSprite("../pacman.png");
 
-	Data::ClientsText = Renderer::CreateText(Data::Font, "Clients connected:", 24);
+	Data::ClientsText =
+		Renderer::CreateText(Data::Font, "Clients connected:", 24);
 
 	Data::JoinData.IP[0] = 127;
 	Data::JoinData.IP[1] = 0;
@@ -71,11 +75,15 @@ int main()
 	Data::JoinData.Port = 0;
 
 	Animation pac_move(4);
-	pac_move.AddFrame(34, 170, 32, 32);
-	pac_move.AddFrame(1, 170, 32, 32);
-	pac_move.AddFrame(34, 170, 32, 32);
-	pac_move.AddFrame(67, 170, 32, 32);
-	Data::GameplayData.PacSprite.Animations.push_back(pac_move);
+	//pac_move.AddFrame(34, 170, 32, 32);
+	//pac_move.AddFrame(1, 170, 32, 32);
+	//pac_move.AddFrame(34, 170, 32, 32);
+	//pac_move.AddFrame(67, 170, 32, 32);
+	pac_move.AddFrame(18, 1, 16, 16);
+	pac_move.AddFrame(35, 1, 16, 16);
+	pac_move.AddFrame(18, 1, 16, 16);
+	pac_move.AddFrame(1, 1, 16, 16);
+	Data::GameplayData.PacmanSprite.Animations.push_back(pac_move);
 	Data::GameplayData.AnimFrame = 0;
 
 	//Animation gho_move(4);
@@ -158,10 +166,10 @@ static void render(const MainState &state)
 		for (unsigned int i = 0; i < menu.size(); i++)
 		{
 			Renderer::DrawText(
-					TextMap[Menu_EN[menu[i].Text]], 6, 10 + 6 * i);
+					TextMap[Menu_EN[menu[i].Text]], 10, 10 + 12 * i);
 			if (i == index)
 			{
-				Renderer::DrawText(TextMap[">"], 2, 10 + 6 * i);
+				Renderer::DrawText(TextMap[">"], 2, 10 + 12 * i);
 			}
 		}
 	}
@@ -178,17 +186,17 @@ static void render(const MainState &state)
 		for (unsigned int i = 0; i < str.size(); i++)
 		{
 			std::string s({ str[i] });
-			Renderer::DrawText(TextMap[s], 10 + 4 * i, 15);
+			Renderer::DrawText(TextMap[s], 10 + 8 * i, 15);
 		}
 
-		Renderer::DrawText(clients_text, 14, 20);
+		Renderer::DrawText(clients_text, 14, 25);
 		ss.str("");
 		ss << lobby_count;
 		str = ss.str();
 		for (unsigned int i = 0; i < str.size(); i++)
 		{
 			std::string s({ str[i] });
-			Renderer::DrawText(TextMap[s], 80 + 4 * i, 20);
+			Renderer::DrawText(TextMap[s], 140 + 8 * i, 25);
 		}
 	}
 	else if (state == Join)
@@ -208,14 +216,14 @@ static void render(const MainState &state)
 		for (unsigned int i = 0; i < disp.size(); i++)
 		{
 			std::string s({ disp[i] });
-			Renderer::DrawText(TextMap[s], 10 + 4 * i, 15);
+			Renderer::DrawText(TextMap[s], 10 + 8 * i, 15);
 		}
 		int iPos = index;
 		if (index > 2) iPos++;
 		if (index > 5) iPos++;
 		if (index > 8) iPos++;
 		if (index > 11) iPos++;
-		Renderer::DrawText(TextMap["^"], 10 + 4 * iPos, 20);
+		Renderer::DrawText(TextMap["^"], 10 + 8 * iPos, 25);
 	}
 	else if (state == ClientWaiting)
 	{
