a507de0d38e2bb800103d39bf70a431732cfbdd9
Aneesh Roy <roy.aneesh@gmail.com>
2015-11-29 22:51:20 +0000

    Altered math to support even tile sizes

--- Field.cpp
+++ Field.cpp
@@ -45,15 +45,19 @@ Field::Field(std::string file)
 
 Field::TileType Field::InterpolateAtPos(int x, int y) const
 {
+	// Looks confusing, but all it's doing is checking a half-tile neighborhood
+	// of the given position and doing a bitwise AND (so walls have priority
+	// over open space).
 	int w = FIELD_WIDTH * TILE_SIZE;
 	int h = FIELD_HEIGHT * TILE_SIZE;
-	int del = (TILE_SIZE - 1) / 2;
+	int d_sm = (TILE_SIZE - 1) / 2;
+	int d_lg = TILE_SIZE / 2;
 	std::size_t x_c = (x + w) % w;
-	std::size_t x_r = (x_c + del) % w;
-	std::size_t x_l = (x_c - del + w) % w;
+	std::size_t x_r = (x_c + d_lg) % w;
+	std::size_t x_l = (x_c - d_sm + w) % w;
 	std::size_t y_c = (y + h) % h;
-	std::size_t y_d = (y_c + del) % h;
-	std::size_t y_u = (y_c - del + h) % h;
+	std::size_t y_d = (y_c + d_lg) % h;
+	std::size_t y_u = (y_c - d_sm + h) % h;
 	TileType t5 = Tiles[x_c / TILE_SIZE][y_c / TILE_SIZE];
 	TileType t6 = Tiles[x_r / TILE_SIZE][y_c / TILE_SIZE];
 	TileType t9 = Tiles[x_r / TILE_SIZE][y_u / TILE_SIZE];
